8286fe68d7ea209e87874e191ed00e15d00e12d2
rtrice <rtamberli@gmail.com>
2014-01-01 15:17:18 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -9,6 +9,15 @@ if __name__ == '__main__':
     host = os.environ.get('OPENSHIFT_DIY_IP', '127.0.0.1')
     port = os.environ.get('OPENSHIFT_DIY_PORT', '8080')
     logging.info('local python application serving at %s:%s', host, port)
-    from gevent.wsgi import WSGIServer
-    WSGIServer((host, int(port)), application).serve_forever()
+    from gunicorn.app.wsgiapp import WSGIApplication
+    class GunicornApplication(WSGIApplication):
+        def init(self, parser, opts, args):
+            return {'bind': '%s:%d' % (host, int(port)),
+                    'workers': 2,
+                    'worker_class': 'gevent'}
+        def load(self):
+            return application
+    GunicornApplication().run()
+
+
 
